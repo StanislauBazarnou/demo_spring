9ddf819d85307b54e200b63ffd29a8231a8ff51a
Stanislau Bazarnou <Stanislau_Bazarnou@epam.com>
2024-02-13 14:22:24 +0400

    add encapsulation: a mechanism for interaction, methods get and put

--- src/main/java/com/example/demo/dao/TicketDao.java
+++ src/main/java/com/example/demo/dao/TicketDao.java
@@ -2,24 +2,25 @@ package com.example.demo.dao;
 
 import com.example.demo.Storage;
 import com.example.demo.model.Ticket;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Map;
 
 public class TicketDao {
-    private final Map<Long, Ticket> ticketMap;
+    private final Storage storage;
 
-    @Autowired
-    public TicketDao(Storage database) {
-        this.ticketMap = database.getTicketMap();
+    // one constructor - @Autowired is not required
+    public TicketDao(Storage storage) {
+        this.storage = storage;
     }
 
-    public Ticket save(Ticket ticket) {
-        ticketMap.put(ticket.getUserId(), ticket);
-        return ticket;
+    public Ticket getTicketById(Long id) {
+        return storage.getTicketMap().get(id);
     }
 
-    public Ticket getById(long id) {
-        return ticketMap.get(id);
+    public void putTicket(Long id, Ticket ticket) {
+        storage.getTicketMap().put(id, ticket);
     }
+
+    public Ticket removeTicket(Long id) {
+        return storage.getTicketMap().remove(id);
+    }
+
 }
